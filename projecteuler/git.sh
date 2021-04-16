c1f89f6af117399f9d0feb0fe60d258687252e4f
Akash671 <akashsaini454545gmail.com>
2021-04-16 23:36:19 +0530

    Akash

--- projecteuler/git.sh
+++ projecteuler/git.sh
@@ -4,13 +4,17 @@ echo "$(tput setaf 31)|            @Akash671            |"
 echo "$(tput setaf 31)|                                 |"
 echo "$(tput setaf 31)+---------------------------------+"
 echo "$(tput setaf 21)-----------Hello Akash! welcome in gihub-------------"
-echo "$(tput setaf 46)+-----------------+ "+--------------------------+
+echo "$(tput setaf 46)+-----------------+ +--------------------------+"
 read -p "| Enter file name |:|" name
-echo "$(tput setaf 46)+-----------------+ "+--------------------------+
+echo "$(tput setaf 46)+-----------------+ +--------------------------+"
 git add $name
-read -p "Enter commit message : " message
+
+echo "$(tput setaf 10)+----------------------+ +---------------------+"
+read -p "| Enter commit message |:|" message
+echo "$(tput setaf 10)+----------------------+ +---------------------+"
 git commit -m "$message"
 echo "$(tput setaf 41)Your message is : $message"
+
 git push
 echo "$(tput setaf 31)+---------------------------------+"
 echo "$(tput setaf 31)|                                 |"
